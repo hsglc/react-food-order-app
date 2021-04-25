a0b5492b10d0ed0e29e501e85584421a738b593b
huseyin gulcu <huseyingulcuu@gmail.com>
2021-04-25 16:11:02 +0300

    intented components have generated and ui has implemented

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+function App() {
+  return (
+    <>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
