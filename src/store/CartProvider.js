2e390f7a2a04f96e05c1c79e0a1c639acb50b57a
huseyin gulcu <huseyingulcuu@gmail.com>
2021-05-05 18:47:54 +0300

    in cartprovider component, action type 'clear's bug has resolved

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -24,34 +24,40 @@ const cartReducer = (state, action) => {
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
+      
       updatedItems = state.items.concat(action.item);
     }
 
-    if ((action.type = "CLEAR")) {
-      return defaultCartState;
-    }
+    
+  
 
-    return defaultCartState;
-  }
-
-  if (action.type === "REMOVE") {
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
-    );
-    const existingItem = state.items[existingCartItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.price;
-    let updatedItems;
-    if (existingItem.amount === 1) {
-      updatedItems = state.items.filter((item) => item.id !== action.id);
-    } else {
-      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
-    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
+  }
+
+  if(action.type==='REMOVE'){
+
+    const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if(existingItem.amount === 1){
+      updatedItems = state.items.filter(item => item.id !== action.id );
+    }else{
+      const updatedItem = {...existingItem, amount:existingItem.amount -1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return{
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+
+  if(action.type === "REMOVE"){
+    return defaultCartState;
   }
 
   return defaultCartState;
@@ -71,8 +77,8 @@ const CartProvider = (props) => {
   };
 
   const clearCartHandler = () => {
-    dispatchCartAction({ type: "CLEAR" });
-  };
+    dispatchCartAction({type:'CLEAR'})
+  }
 
   const cartContext = {
     items: cartState.items,
